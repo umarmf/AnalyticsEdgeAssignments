b64c759dd651a6cf19a980e1c55aa282ef46e2b9
umarmf <umarmf@users.noreply.github.com>
2018-08-03 09:32:36 +0700

    finished vid4

--- Unit 2/Intro/vid4.R
+++ Unit 2/Intro/vid4.R
@@ -1,3 +1,18 @@
 read.csv("wine.csv")
 wine <- read.csv("wine.csv")
 wine_test <- read.csv("wine_test.csv")
+str(wine)
+summary(wine)
+model1 <- lm(Price~AGST,data=wine)
+summary(model1)
+model1$residuals
+sse <- sum(model1$residuals^2)
+sse
+model2 <- lm(Price~AGST+HarvestRain,data=wine)
+summary(model2)
+sse <- sum(model2$residuals^2)
+sse
+model3 <- lm(Price~AGST+HarvestRain+WinterRain+Age+FrancePop,data=wine)
+summary(model3)
+sse <- sum(model3$residuals^2)
+sse
